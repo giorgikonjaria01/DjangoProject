bf2f9538536927c7c71a82b7c7b092fdc1ca274d
EleGreekBlock <elegreekblocks@gmail.com>
2026-07-13 21:37:16 +0400

    Added user authentication

--- finance-tracker/apps/accounts/views.py
+++ finance-tracker/apps/accounts/views.py
@@ -1,3 +1,63 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+from .models import User
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Username already exists")
+            return redirect("register")
+
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=password
+        )
+
+        login(request, user)
+
+        messages.success(
+            request,
+            "Account created successfully"
+        )
+
+        return redirect("dashboard")
+
+    return render(request, "accounts/register.html")
+
+
+def login_view(request):
+
+    if request.method == "POST":
+
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+
+        if user:
+            login(request, user)
+            return redirect("dashboard")
+
+        messages.error(
+            request,
+            "Invalid credentials"
+        )
+
+    return render(request, "accounts/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
--- finance-tracker/config/urls.py
+++ finance-tracker/config/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.shortcuts import redirect
+
+
+def home(request):
+    return redirect("login")
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("", home, name="home"),
+    path("admin/", admin.site.urls),
+    path("accounts/", include("apps.accounts.urls")),
 ]
